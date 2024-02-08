a697df0422c999fc52b62199111f144a20d772e5
DANIEL HAROLDO REYES GARCIA <dreyesg11@miumg.edu.gt>
2024-02-07 22:12:48 -0600

    Version 1

---
+++ src/main/java/com/mycompany/tarea_registropacientes/Tarea_registroPacientes.java
@@ -0,0 +1,12 @@
+package com.mycompany.tarea_registropacientes;
+
+/**
+ *
+ * @author 1214k
+ */
+public class Tarea_registroPacientes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
