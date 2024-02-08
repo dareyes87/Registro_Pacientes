0aec939ad6d07f371367e94cf361d613f4455b9a
DANIEL HAROLDO REYES GARCIA <dreyesg11@miumg.edu.gt>
2024-02-08 17:37:04 -0600

    Actualizacion con base de datos

--- src/main/java/com/mycompany/tarea_registropacientes/Tarea_registroPacientes.java
+++ src/main/java/com/mycompany/tarea_registropacientes/Tarea_registroPacientes.java
@@ -1,5 +1,8 @@
 package com.mycompany.tarea_registropacientes;
 
+import com.formdev.flatlaf.intellijthemes.FlatCarbonIJTheme;
+import ventanas.login;
+
 /**
  *
  * @author 1214k
@@ -8,5 +11,10 @@ public class Tarea_registroPacientes {
 
     public static void main(String[] args) {
         System.out.println("Empezando la tarea de registro pacientes");
+        FlatCarbonIJTheme.setup();
+        login loggearse = new login();
+        loggearse.setVisible(true);
+        
+        
     }
 }
